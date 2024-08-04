9e0a0e6ce68e7bcc30bf245919f88fc38d902628
made-in-hongkong <info@made-in-hongkong.space>
2024-08-04 20:04:58 +0100

    Update script.js

--- blog/script.js
+++ blog/script.js
@@ -1,5 +1,5 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Form submission is currently not available on this static site.');
+    alert('Form submission is currently not available on this site.');
     // Implement a service like Formspree for form submissions.
 });
