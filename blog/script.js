1a498f987d6f62b793bc115325e67b45e89899dc
made-in-hongkong <info@made-in-hongkong.space>
2024-08-04 22:03:51 +0100

    Update script.js

--- blog/script.js
+++ blog/script.js
@@ -1,12 +1,28 @@
 // Initialize EmailJS with your user ID
-emailjs.init("info@made-in-hongkong.com");
+emailjs.init({
+    publicKey: 'u2A_7Myddbi0-0ii1',
+    // Do not allow headless browsers
+    blockHeadless: true,
+    blockList: {
+      // Block the suspended emails
+      list: ['foo@emailjs.com', 'bar@emailjs.com'],
+      // The variable contains the email address
+      watchVariable: 'userEmail',
+    },
+    limitRate: {
+      // Set the limit rate for the application
+      id: 'app',
+      // Allow 1 request per 10s
+      throttle: 10000,
+    },
+  });
 
 // Add an event listener to the form submission
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission behavior
 
     // Send the form data using EmailJS
-    emailjs.sendForm('service_62qai5b', 'template_tim2m1b', this)
+    emailjs.send('service_62qai5b', 'template_tim2m1b', this)
         .then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
             alert('Message sent successfully!');
