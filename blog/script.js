a52f5b7db7944ff2957c10ccd0dcf7c6c457de2c
made-in-hongkong <info@made-in-hongkong.space>
2024-08-04 21:31:03 +0100

    Update script.js

--- blog/script.js
+++ blog/script.js
@@ -1,5 +1,19 @@
+// Initialize EmailJS with your user ID
+emailjs.init("info@made-in-hongkong.com");
+
+// Add an event listener to the form submission
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Form submission is currently not available on this site.');
-    // Implement a service like Formspree for form submissions.
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Send the form data using EmailJS
+    emailjs.sendForm('service_62qai5b', 'template_tim2m1b', this)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Message sent successfully!');
+            // Optionally, you can reset the form here
+            document.getElementById('contact-form').reset();
+        }, function(error) {
+            console.error('FAILED...', error);
+            alert('Failed to send message. Please try again later.');
+        });
 });
